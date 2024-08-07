644d8c8bbaec852acd30066a686cc13f4bb64313
cmzsbql <167775435+cmzsbql@users.noreply.github.com>
2024-08-07 18:20:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,3 +9,5 @@
 
 export CUDA_VISIBLE_DEVICES="1"
 deepspeed  train_arlm.py --exp-name ARTM --batch-size 4096 --num-layers 1  --embed-dim-gpt 1024 --width 512 --code-dim 512 --nb-code 256  --n-head-gpt 16 --block-size 6 --ff-rate 4 --drop-out-rate 0.1 --resume-pth ./output/output_stock/VQVAE/net_best_ds.pth --vq-name VQVAE --out-dir ./output/output_stock/ --total-iter 20000 --lr-scheduler 150000 --lr 0.0008 --dataname stock --down-t 2 --depth 3 --quantizer ema_reset2 --eval-iter 2500 --print-iter 500 --pkeep 0.7 --dilation-growth-rate 3 --vq-act relu --window-size 24  &
+
+###
